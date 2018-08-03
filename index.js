0ccb8005e219a8af162688cafb94ebbb7dd1719c
Katie Gorbell <katiegorbell@gmail.com>
2018-08-03 11:22:11 -0500

    added get request for projects

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const server = express();
 
+const projectsDb = require('./data/helpers/projectModel.js');
+const actionsDb = require('./data/helpers/actionModel.js');
+
 server.use(express.json());
 
+// ===== ERROR MIDDLEWARE =====
 
+const serverErrorMsg = () => {
+    res.status(500).json({ error: 'SERVER ERROR!' })
+}
+
+// ===== MIDDLEWARE =====
+
+
+// ===== PROJECT REQUESTS =====
+
+server.get('/projects', (req, res) => {
+    projectsDb.get()
+        .then( response => {
+            res.status(200).json(response)
+        })
+        .catch(() => {
+            serverErrorMsg();
+        })
+})
+
+
+// ===== ACTION REQUESTS =====
 
 
 
