a96fd32383b26f26afa6a32857b7266e2e0a0130
Katie Gorbell <katiegorbell@gmail.com>
2018-08-03 11:32:14 -0500

    get request for individual projects added

--- index.js
+++ index.js
@@ -27,6 +27,21 @@ server.get('/projects', (req, res) => {
         })
 })
 
+server.get('/projects/:id', (req, res) => {
+    const { id } = req.params;
+
+    projectsDb.get(id)
+        .then(response => {
+            if (response.length == 0) {
+                res.status(404).json({ message: 'The specified project could not be found' })
+            }
+            res.status(200).json(response)
+        })
+        .catch(() => {
+            serverErrorMsg();
+        })
+})
+
 
 // ===== ACTION REQUESTS =====
 
